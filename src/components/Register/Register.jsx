79d625a5e3ee23b324604be058f0d58cbe8c615e
kabirshuvo <kabirshuvo19@gmail.com>
2023-04-15 02:37:27 +0600

    Password Validation Improved

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -18,6 +18,20 @@ const Register = () => {
         const email = event.target.email.value;
         const password = event.target.password.value;
         console.log(email, password);
+        //*email validation
+        if(!/^(?=.*[A-Z]).+$/.test(password)){
+            setError('Please add atleast 1 uppercase');
+            return
+          } else if (!/(?=.*\d)/.test(password)) {
+            setError("Password should contain at least one number") ;
+            return ;
+          } else if (!/(?=.*[!@#$%^&*])/.test(password)) {
+            setError("Password should contain at least one special character") ;
+            return ;
+          } else if (password.length < 7){
+            setError('Password Must be at least more than 7 carecter')
+            return
+          }
         // create User in fireBase
         createUserWithEmailAndPassword(auth, email, password)
         .then(result => {
