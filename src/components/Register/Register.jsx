12dcce84c5f00c5c8be30059ba4da518194eee1e
kabirshuvo <kabirshuvo19@gmail.com>
2023-04-15 00:24:06 +0600

    Registration form updated

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,6 +1,9 @@
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import React, { useState } from 'react';
+import { Button, Card, Form } from 'react-bootstrap';
+import { FaGithub, FaGoogle, FaLinkedin, FaTwitter } from 'react-icons/fa';
 import app from '../../firebase/firebase.config';
+
 
 const Register = () => {
     const [email, setEmail] = useState();
@@ -34,17 +37,54 @@ const Register = () => {
     
 
     return (
-        <div >
-            <h4>Please Register</h4>
+        <Card style={{ width: '20rem', float: 'right' }}>
+        <Card.Body>
+        <Card.Title style={{textAlign: 'center', marginBottom: '1rem', fontSize: '2rem', color: '#007bff'}}>Please Register</Card.Title>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formBasicEmail" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <Form.Label style={{ textAlign: 'center', marginBottom: '1rem', marginTop: '1rem', fontSize: '1rem', color: '#007bff', transition: 'color 0.2s ease-in-out' }}
+  onMouseEnter={(e) => e.target.style.color = '#6c757d'}
+  onMouseLeave={(e) => e.target.style.color = '#007bff'}
+>
+  Email address
+</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" onChange={handleEmailChange} />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <Form.Label style={{ textAlign: 'center', marginBottom: '1rem', marginTop: '1rem', fontSize: '1rem', color: '#007bff', transition: 'color 0.2s ease-in-out' }}
+  onMouseEnter={(e) => e.target.style.color = '#6c757d'}
+  onMouseLeave={(e) => e.target.style.color = '#007bff'}
+>
+  Email address
+</Form.Label>
+            <Form.Control type="password" placeholder="Password should be more than 6 characters" onBlur={handlePasswordBlur} />
+            </Form.Group>
+            <Button variant="primary" type="submit" style={{ marginTop: '1rem' }}>
+  Register
+</Button>
+            <hr />
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+  <Button variant="light" style={{ margin: '.5rem' }}>
+    <FaGoogle style={{ marginRight: '.5rem' }} />
+    Register with Google
+  </Button>
+  <Button variant="light" style={{ margin: '.5rem' }}>
+    <FaGithub style={{ marginRight: '.5rem' }} />
+    Register with GitHub
+  </Button>
+  <Button variant="light" style={{ margin: '.5rem' }}>
+    <FaTwitter style={{ marginRight: '.5rem' }} />
+    Register with Twitter
+  </Button>
+  <Button variant="light" style={{ margin: '.5rem' }}>
+    <FaLinkedin style={{ marginRight: '.5rem' }} />
+    Register with LinkedIn
+  </Button>
+</div>
 
-            <form onSubmit={handleSubmit} style={{display: 'flex', flexDirection: 'column', gap: '.1rem'}}>
-                <input onChange={handleEmailChange} type="email" name='email' id='email' placeholder='Your Email Here' />
-                <br />
-                <input onBlur={handlePasswordBlur} type="password" name='password' id='password' placeholder='password should be more than 6 charecter' />
-                <br />
-                <input type="submit" value={'Register'}></input>
-            </form>
-        </div>
+          </Form>
+        </Card.Body>
+      </Card>
     );
 };
 
