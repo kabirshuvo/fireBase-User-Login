6a271b7a8d0f0c50a1c872bd64df7549b57eb3fb
kabirshuvo <kabirshuvo19@gmail.com>
2023-04-15 02:07:02 +0600

    Error and success message is implimented

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
-
+import { Button } from 'react-bootstrap';
+import { FaGithub, FaGoogle, FaLinkedin, FaTwitter } from 'react-icons/fa';
 const Login = () => {
     return (
         <div>
             <h1>Login Page</h1>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <Button variant="light" style={{ margin: '.5rem' }}>
+                    <FaGoogle style={{ marginRight: '.5rem' }} />
+                            Register with Google
+                    </Button>
+                    <Button variant="light" style={{ margin: '.5rem' }}>
+                    <FaGithub style={{ marginRight: '.5rem' }} />
+                            Register with GitHub
+                    </Button>
+                    <Button variant="light" style={{ margin: '.5rem' }}>
+                    <FaTwitter style={{ marginRight: '.5rem' }} />
+                            Register with Twitter
+                    </Button>
+                    <Button variant="light" style={{ margin: '.5rem' }}>
+                    <FaLinkedin style={{ marginRight: '.5rem' }} />
+                            Register with LinkedIn
+                    </Button>
+            </div>
         </div>
     );
 };
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,32 +1,42 @@
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import React, { useState } from 'react';
-import { Button, Card, Form } from 'react-bootstrap';
+import { Alert, Button, Card, Form } from 'react-bootstrap';
 import { FaGithub, FaGoogle, FaLinkedin, FaTwitter } from 'react-icons/fa';
 import app from '../../firebase/firebase.config';
 
-
+const auth = getAuth(app)
 const Register = () => {
-    const [email, setEmail] = useState();
-    const auth = getAuth(app)
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+   
 
     const handleSubmit = (event) => {
+        // Prevent to page refresh;
         event.preventDefault();
+        setSuccess('');
+        // collect form data
         const email = event.target.email.value;
         const password = event.target.password.value;
         console.log(email, password);
+        // create User in fireBase
         createUserWithEmailAndPassword(auth, email, password)
         .then(result => {
-            const user = result.user;
-            console.log(user);
+            const loggedUser = result.user;
+            console.log(loggedUser);
+            setError('')
+            event.target.reset();
+            setSuccess('Welcome to Xadu.life');
         }).catch(error =>{
-            console.error(error) 
+            console.error(error.message);
+            setError(error.message);
+            
         } )
         
     }
 
     const handleEmailChange = (event) => {
             // console.log(event.target.value)
-            setEmail(event.target.value)
+            // setEmail(event.target.value)
     }
     
     const handlePasswordBlur = (event) => {
@@ -43,47 +53,48 @@ const Register = () => {
           <Form onSubmit={handleSubmit}>
             <Form.Group controlId="formBasicEmail" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <Form.Label style={{ textAlign: 'center', marginBottom: '1rem', marginTop: '1rem', fontSize: '1rem', color: '#007bff', transition: 'color 0.2s ease-in-out' }}
-  onMouseEnter={(e) => e.target.style.color = '#6c757d'}
-  onMouseLeave={(e) => e.target.style.color = '#007bff'}
->
-  Email address
-</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" onChange={handleEmailChange} />
+                onMouseEnter={(e) => e.target.style.color = '#6c757d'}
+                onMouseLeave={(e) => e.target.style.color = '#007bff'}
+
+            >  Email address* </Form.Label>
+
+            <Form.Control type="email" placeholder="Enter email" onChange={handleEmailChange} name='email' required />
             </Form.Group>
             <Form.Group controlId="formBasicPassword" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <Form.Label style={{ textAlign: 'center', marginBottom: '1rem', marginTop: '1rem', fontSize: '1rem', color: '#007bff', transition: 'color 0.2s ease-in-out' }}
-  onMouseEnter={(e) => e.target.style.color = '#6c757d'}
-  onMouseLeave={(e) => e.target.style.color = '#007bff'}
->
-  Email address
-</Form.Label>
-            <Form.Control type="password" placeholder="Password should be more than 6 characters" onBlur={handlePasswordBlur} />
+                onMouseEnter={(e) => e.target.style.color = '#6c757d'}
+                onMouseLeave={(e) => e.target.style.color = '#007bff'}
+
+            >Password* </Form.Label>
+
+            <Form.Control type="password" placeholder="Password should be more than 6 characters" onBlur={handlePasswordBlur} name='password' required/>
             </Form.Group>
-            <Button variant="primary" type="submit" style={{ marginTop: '1rem' }}>
-  Register
-</Button>
+            {error && <Alert variant="danger" style={{color: 'red'}}>{error}</Alert>}
+            {success && <Alert variant="danger" style={{color: 'green'}}>{success}</Alert>}
+            <Button variant="primary" type="submit" style={{ marginTop: '.2rem' }}>Register </Button>
             <hr />
             <div style={{ display: 'flex', justifyContent: 'center' }}>
-  <Button variant="light" style={{ margin: '.5rem' }}>
-    <FaGoogle style={{ marginRight: '.5rem' }} />
-    Register with Google
-  </Button>
-  <Button variant="light" style={{ margin: '.5rem' }}>
-    <FaGithub style={{ marginRight: '.5rem' }} />
-    Register with GitHub
-  </Button>
-  <Button variant="light" style={{ margin: '.5rem' }}>
-    <FaTwitter style={{ marginRight: '.5rem' }} />
-    Register with Twitter
-  </Button>
-  <Button variant="light" style={{ margin: '.5rem' }}>
-    <FaLinkedin style={{ marginRight: '.5rem' }} />
-    Register with LinkedIn
-  </Button>
-</div>
+                    <Button variant="light" style={{ margin: '.5rem' }}>
+                    <FaGoogle style={{ marginRight: '.5rem' }} />
+                            Register with Google
+                    </Button>
+                    <Button variant="light" style={{ margin: '.5rem' }}>
+                    <FaGithub style={{ marginRight: '.5rem' }} />
+                            Register with GitHub
+                    </Button>
+                    <Button variant="light" style={{ margin: '.5rem' }}>
+                    <FaTwitter style={{ marginRight: '.5rem' }} />
+                            Register with Twitter
+                    </Button>
+                    <Button variant="light" style={{ margin: '.5rem' }}>
+                    <FaLinkedin style={{ marginRight: '.5rem' }} />
+                            Register with LinkedIn
+                    </Button>
+            </div>
 
           </Form>
         </Card.Body>
+       
       </Card>
     );
 };
